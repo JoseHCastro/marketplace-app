b18b04bdd742aab53ae9ad96057de69b8a8c92bf
Jose <josehco85@gmail.com>
2024-05-10 17:36:11 -0400

    Roles y Permisos
    Los permisos no se estaban asignando al rol (lo revisé en la bd), solo hice ejemplo para usuarios, USAR PERMISOS para filtrar las acciones en la VISTA blade.php, en las RUTAS web.php y en la VISTA del menu de navegacion en adminlte.php.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,37 +19,43 @@ class UserSeeder extends Seeder
             'name'=>'Miguel Ángel Arteaga Guzmán',
             'email'=>'miguel@gmail.com',
             'password'=>Hash::make('12345678'),
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
         //2
         $usuario = User::create([
             'name'=>'Roque Mauricio Banegas Lopez',
             'email'=>'mauricio@gmail.com',
             'password'=>Hash::make('12345678'),
 
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
         //3
         $usuario = User::create([
             'name'=>'Jose Humberto Castro Ortiz',
             'email'=>'josehco85@gmail.com',
             'password'=>Hash::make('123'),
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
         //4
         $usuario = User::create([
             'name'=>'Rodrigo Centellas Delgadillo',
             'email'=>'rodrigo@gmail.com',
             'password'=>Hash::make('123'),
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
         //5
         $usuario = User::create([
             'name'=>'Abel Alejandro López Cabero',
             'email'=>'abel@gmail.com',
             'password'=>Hash::make('12345678'),
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
         //6
         $usuario = User::create([
             'name'=>'Tania Valda Donozo',
             'email'=>'tania@gmail.com',
             'password'=>Hash::make('12345678'),
-        ]);//->assignRole('administrador');
+        ])->assignRole('administrador');
+        //7
+        $usuario = User::create([
+            'name'=>'Usuario Apellido',
+            'email'=>'usuario@gmail.com',
+            'password'=>Hash::make('12345678'),
+        ])->assignRole('usuario');
     }
 }
